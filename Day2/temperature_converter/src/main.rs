bad07895ee841ec4677128f120be9d36f7432f70
Gustavo Garabal Rivadulla <gustavogarabal@gmail.com>
2025-10-15 16:38:53 +0200

    Funcional. Added methods to parse temperatures. TODO: Loops to repeat the execution when inputs are incorrect

--- Day2/temperature_converter/src/main.rs
+++ Day2/temperature_converter/src/main.rs
@@ -1,3 +1,55 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Temperature Converter");
+    println!("1- Celsius to Fahrenheit");
+    println!("2- Fahrenheit to Celsius");
+    println!("Please select an option (1 or 2):");
+
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("Failed to read input");
+
+    let choice:u8 = match choice.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid choice. Please enter 1 or 2");
+            return;
+        }
+    };
+
+    if choice == 1 {
+        celsius_to_farenheit()
+    } else if choice == 2 {
+        farenheit_to_celsius()
+    } else {
+        println!("Invalid choice. Please enter 1 or 2");
+    }
+}
+
+fn celsius_to_farenheit() {
+    let celsius = get_temperature("Celsius");
+    let fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+    println!("{:.2}º Celsius is {:.2}º Fahrenheit", celsius, fahrenheit);
+}
+
+fn farenheit_to_celsius() {
+    let fahrenheit = get_temperature("Fahrenheit");
+    let celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+    println!("{:.2}º Fahrenheit is {:.2}º Celsius", fahrenheit, celsius);
+}
+
+fn get_temperature(cadena:&str) -> f64 {
+    println!("Enter temperature in {}: ", cadena);
+    let mut temp = String::new();
+    io::stdin().read_line(&mut temp).expect("Failed to read input");
+
+    let temp:f64 = match temp.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid temperature. Please enter a number.");
+            return 0.0;
+        }
+    };
+
+    return temp;
 }
